dba951c4ab89eba885bbf51b1e43da8e19cfda09
Leaf114514 <94676841+Leaf114514@users.noreply.github.com>
2021-12-22 21:49:45 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -15,5 +15,5 @@
 
 # Add a feed source
 echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
-#echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
 echo 'src-git 123456 https://github.com/OpenWyu/luci-app-esurfing-client.git' >>feeds.conf.default
